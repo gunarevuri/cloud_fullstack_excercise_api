0a02d97fd3243557f4eb8989e0cb6476b712d58d
gunarevuri <gunarevuri@gmail.com>
2020-05-10 21:45:32 +0530

    intitial

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramdev",
+    "password": "udagramdev",
+    "database": "udagramdev",
+    "host": "udagramdev.ckagy8ompupl.ap-south-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "ap-south-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-guna-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
